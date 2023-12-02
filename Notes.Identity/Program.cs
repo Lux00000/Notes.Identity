dc2b9fdb44a5eb3d9fe5185c89d6f58b3661eff6
Goudini <luxmain000@gmai.com>
2023-12-02 03:10:13 +0300

    Add project files.

---
+++ Notes.Identity/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Identity.Data;
+namespace Notes.Identity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build(); 
+            using(var scope = host.Services.CreateScope()) 
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<AuthDbContext>();
+                    DbInitializer.Initialize(context);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while app initialization");
+                }
+            }
+            host.Run();
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ Notes.Identity/Startup.cs
@@ -0,0 +1,78 @@
+﻿using IdentityServer4.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
+using System.Data.Common;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace Notes.Identity
+{
+    public class Startup
+    {
+        public IConfiguration AppConfiguration { get; }
+
+        public Startup(IConfiguration configuration ) =>
+            AppConfiguration = configuration;
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = AppConfiguration.GetValue<string>("DbConnection");
+
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+    
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 5;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+
+            }).AddEntityFrameworkStores<AuthDbContext>().AddDefaultTokenProviders();
+
+            services.AddIdentityServer()
+                .AddAspNetIdentity<AppUser>()
+                .AddInMemoryApiResources(Configuration.ApiResources)
+                .AddInMemoryIdentityResources(Configuration.IdentityResources)
+                .AddInMemoryApiScopes(Configuration.ApiScopes)
+                .AddInMemoryClients(Configuration.Clients)
+                .AddDeveloperSigningCredential();
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Notes.Identity.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+            });
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "Styles")),
+                RequestPath = "/styles"
+            });
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
